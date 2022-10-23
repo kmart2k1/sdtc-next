f3596d28bfae183c4a7b9853e83a78a0ded43916
keith-mar <keith.mar@govx.com>
2022-10-22 21:18:57 -0700

    initial

---
+++ sdtc-ui/pages/_app.js
@@ -0,0 +1,12 @@
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <div>sdtc</div>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
