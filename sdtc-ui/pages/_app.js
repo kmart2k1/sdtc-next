5acaf9d8869da654a1d075ab276482c69a61d56d
keith-mar <keith.mar@govx.com>
2022-10-22 22:27:47 -0700

    members

--- sdtc-ui/pages/_app.js
+++ sdtc-ui/pages/_app.js
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script strategy="beforeInteractive">
+        {`console.log('hi');var MemberSpace = window.MemberSpace || {"subdomain":"onrender"};
+(function(d){
+  var s = d.createElement("script");
+  s.src = "https://cdn.memberspace.com/scripts/widgets.js";
+  var e = d.getElementsByTagName("script")[0];
+  e.parentNode.insertBefore(s,e);
+}(document));
+      `}
+      </Script>
       <div>sdtc</div>
       <Component {...pageProps} />
     </>
